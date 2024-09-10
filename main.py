794323bacec0851ac7ce8daefe2eba530ac55244
tahmid235 <ttahmidchowdhury2010@gmail.com>
2024-09-10 21:53:37 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+try:
+    pass
+except SyntaxError:
+    print("Please check your syntax!")
+except ValueError:
+    print("Please check your value!")
+except NameError:
+    print("Please check your variable!")
+except TypeError:
+    print("Please check your typing!")    
+except ZeroDivisionError:
+    print("You cna't devide by zero!")     
+ 
